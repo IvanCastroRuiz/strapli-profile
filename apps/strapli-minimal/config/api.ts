51399c81a4a14a0051741384dd97a7902821c274
IvanCastroRuiz <ivan.castro@devitech.com.co>
2025-10-23 18:37:12 -0500

    feat: depurando lel proyecto

--- apps/strapli-minimal/config/api.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  rest: {
-    prefix: '/api',
-    defaultLimit: 25,
-    maxLimit: 100,
-    withCount: true,
-  },
-};
-
--- apps/strapli-minimal/dist/config/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ env, }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int ? env.int('PORT', 1337) : Number(env('PORT', 1337)),
-    app: {
-        keys: env.array ? env.array('APP_KEYS', ['key1', 'key2']) : String(env('APP_KEYS', 'key1,key2')).split(','),
-    },
-});
